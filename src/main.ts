1528cae83a35f4f89c0d77920f07e1e1fbfdad6e
Amir <far20022002213@gmail.com>
2024-04-29 19:14:46 +0500

    feat: added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as process from 'process';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('todos API')
+    .setDescription('бэк для тудушек')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
--- src/todos/dbModel/todos.ts
+++ src/todos/dbModel/todos.ts
@@ -5,17 +5,21 @@ import {
   PrimaryKey,
   Table,
 } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Table({ tableName: 'todos' })
 export class TodoModel extends Model<TodoModel> {
+  @ApiProperty({ example: 1 })
   @AutoIncrement
   @PrimaryKey
   @Column
   id: number;
 
+  @ApiProperty({ example: 'Сделать уроки(' })
   @Column
   text: string;
 
+  @ApiProperty({ example: false })
   @Column({
     field: 'ischecked',
   })
